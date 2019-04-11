af5a27e98db144b13d548c972cf0af8307487ec2
xuzuhao <1326288580@qq.com>
2019-04-11 15:10:12 +0800

    2019/4/11

--- src/views/Manage/com.vue
+++ src/views/Manage/com.vue
@@ -65,17 +65,7 @@
         </template>
       </el-table-column>
     </el-table>
-    <div class="block">
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="currentPage4"
-        :page-sizes="[10, 20]"
-        :page-size="10"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="3">
-      </el-pagination>
-    </div>
+
   </div>
 </template>
 
--- src/views/Manage/user.vue
+++ src/views/Manage/user.vue
@@ -102,17 +102,7 @@
         </template>
       </el-table-column>
     </el-table>
-    <div class="block">
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="currentPage4"
-        :page-sizes="[10, 20]"
-        :page-size="10"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="5">
-      </el-pagination>
-    </div>
+    
   </div>
 </template>
 
