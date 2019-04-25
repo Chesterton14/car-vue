a9cad7b451c28c98699a9e8ebe80b39d963a093b
xuzuhao <1326288580@qq.com>
2019-04-25 10:49:54 +0800

    修改标题

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
     <div style="text-align: center;width: 100%;height: 100%;">
       <el-card class="box-card" style="margin-top: 50px">
         <div slot="header" class="clearfix">
-          <span><h1>基于Node.js的车联网监控平台</h1></span>
+          <span><h1>基于Node.js的广航车联网监控平台</h1></span>
         </div>
         <div class="text item">
           <h2>车联网监控平台可完成对车辆的实时监控，对车辆行驶信息的分析，实现用户对车辆的信息化管理</h2>
